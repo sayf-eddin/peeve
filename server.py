1e3006329fe8a6a2665bf3d4dfc5e7b5b76c686e
Sayf Ahmed <seahmed@syr.edu>
2021-07-14 17:08:22 -0400

    fixed error

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def redirect():
+def home():
     return redirect("https://twitter.com/caitslobstah")
 
 app.run(host='0.0.0.0', port=environ.get('PORT'))

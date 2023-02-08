6625991eabf97423aab0cf90f5287fc1a082e8a9
Sayf Ahmed <sayfyas25@gmail.com>
2023-02-08 13:28:56 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,11 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from peeve_job import start as run_job
+import requests
 
 sched = BlockingScheduler()
 
 # Runs daily at 7pm UTC
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=19)
-def scheduled_job():
+def tweet_job():
     run_job()
+
+# Keep web instance awake by pinging every 25 mins
+@sched.scheduled_job('interval', day_of_week='mon-sun', seconds=1500)
+def awake_job():
+    r = requests.get('https://pet-peeve.herokuapp.com/')
 
 sched.start()

9fb24dd0adfd0747ff7cce3bc06ce6d803d49dd1
Sayf Ahmed <sayfyas25@gmail.com>
2023-02-08 13:12:09 -0500

    updated time

--- clock.py
+++ clock.py
@@ -3,8 +3,8 @@ from peeve_job import start as run_job
 
 sched = BlockingScheduler()
 
-# Runs daily at 6pm UTC
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18)
+# Runs daily at 7pm UTC
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=19)
 def scheduled_job():
     run_job()
 

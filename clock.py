9d8508c942aae1c337b309974e00dce9a1bf0717
Sayf Ahmed <sayfyas25@gmail.com>
2023-03-21 21:56:32 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,14 +4,19 @@ import requests
 
 sched = BlockingScheduler()
 
+# Keep web instance awake for a bit
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=16, minute=50)
+def awake_job():
+    r = requests.get('https://pet-peeve.herokuapp.com/')
+
 # Runs daily at 5pm UTC, 12pm EST
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=17)
 def tweet_job():
     run_job()
 
 # Keep web instance awake by pinging every 25 minutes (1500 seconds)
-@sched.scheduled_job('interval', seconds=1500)
-def awake_job():
-    r = requests.get('https://pet-peeve.herokuapp.com/')
+# @sched.scheduled_job('interval', seconds=1500)
+# def awake_job():
+#     r = requests.get('https://pet-peeve.herokuapp.com/')
 
 sched.start()
